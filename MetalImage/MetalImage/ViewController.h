c65e20b4ba093b8c632d71cd7907bca8e67fd983
kirinzer <z841354@gmail.com>
2019-07-22 10:24:41 +0800

    Add example metal Image

---
+++ MetalImage/MetalImage/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MetalImage
+//
+//  Created by Kirinzer on 2019/7/22.
+//  Copyright © 2019 kirinzer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
